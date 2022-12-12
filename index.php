b58ab1725f82c5f59a8944dddc692d26604dd0a1
marcodldev <marcodelisi2@gmail.com>
2022-12-12 17:40:15 +0100

    +

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $paragrafo = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.";
 
     <p> <?php echo $paragrafo ?> </p>
 
-    <h5>il paragrafo è lungo <?php strlen($paragrafo) ?></h5>
+    <h5>il paragrafo contiene <?php echo strlen("paragrafo") ?> parole</h5>
 
     <h3>Inserisci la parola che vuoi censurare</h3>
 
@@ -39,9 +39,12 @@ $paragrafo = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.";
 
     <h4>la parola che hai scelto è <?php echo $parola ?>  </h4>
 
-    <pre>
-       var_dump($parola) 
-    </pre>
+    <p> <?php echo $parola2 ?> </p>
+
+    <?php    
+       var_dump($parola2) 
+    
+ ?>
     
 
 </body>
